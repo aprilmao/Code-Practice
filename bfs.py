c01b04e146f569fb810cbac433e3456eb4cf5695
aprilmao <aprilmao129@hotmail.com>
2017-02-03 10:16:47 -0800

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -21,38 +21,19 @@ def bfs_print(head):
             if elementGot.right:
                 thisLevel.put(elementGot.right)
         print
-
-def preOrder(root):
-    if root is None:
-        return
-    print root.data
-    if root.left:
-        preOrder(root.left)
-    if root.right:
-        preOrder(root.right)
-
-def inOrder(root):
-    if root is None:
-        return
-    if root.left:
-        preOrder(root.left)
-    print root.data
-    if root.right:
-        preOrder(root.right)
-
-def postOrder(root):
-    if root is None:
-        return
-    if root.left:
-        preOrder(root.left)
-    if root.right:
-        preOrder(root.right)
-    print root.data
+        
+def bfs(root):
+    
+    bList = []
+    bList.append(root)
+    while len(bList) > 0:
+        print bList[0].data,
+        node = bList.pop(0)
+        if node.left:
+            bList.append(node.left)
+        if node.right:
+            bList.append(node.right)
+            
 
 t = Node(1, Node(2, Node(4, Node(7))), Node(3, Node(5), Node(6)))
 #bfs_print(t)
-preOrder(t)
-print
-inOrder(t)
-print
-postOrder(t)
